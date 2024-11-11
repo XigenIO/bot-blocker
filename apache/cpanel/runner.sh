602eef9ba51c07c06d975d7c85db2d4fbf220a7b
Matthew Bell <matthew.bell@xigen.co.uk>
2024-11-11 15:45:21 +0000

    Update cURL URLS in runner scripts

--- apache/cpanel/runner.sh
+++ apache/cpanel/runner.sh
@@ -1,4 +1,4 @@
-curl -o generate.sh "REPLACE-ME-WITH-GITHUB-URL"
+curl -o generate.sh "https://raw.githubusercontent.com/XigenIO/bot-blocker/refs/heads/master/apache/cpanel/generate.sh"
 chmod +x generate.sh
 ./generate.sh
 rm generate.sh
--- nginx/runner.sh
+++ nginx/runner.sh
@@ -1,4 +1,4 @@
-curl -o generate.sh "REPLACE-ME-WITH-GITHUB-URL"
+curl -o generate.sh "https://raw.githubusercontent.com/XigenIO/bot-blocker/refs/heads/master/nginx/generate.sh"
 chmod +x generate.sh
 ./generate.sh
 rm generate.sh
